5da302241867c215e6ac0bf98afbc422d0a46afb
Akhil Waghmare <akhilwaghmare@gmail.com>
2025-01-02 16:28:02 -0500

    Add basic pulsing button from previous project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
+"use client";
+
+import { motion, useSpring, useTransform } from "framer-motion";
+import { useEffect } from "react";
+
 export default function Home() {
+  const pulse = useSpring(0, { damping: 0, mass: 5, stiffness: 10 });
+  const pulsingBg = useTransform(pulse, (r) => {
+    return `blur(${r}px)`;
+  });
+
+  useEffect(() => {
+    pulse.set(10);
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Starter
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-20 p-24 bg-neutral-900 text-neutral-100">
+      <div className="relative">
+        <button className="relative bg-neutral-700 px-3 py-2 rounded-md hover:bg-neutral-800 transition-colors duration-200 z-10">
+          Subscribe to Built With Code
+        </button>
+        <div
+          className="absolute -inset-[1px] rounded-md"
+          style={{
+            background:
+              "conic-gradient(#ff4545, #00ff99, #006aff, #ff0095, #ff4545)",
+          }}
+        />
+        <motion.div
+          className="absolute -inset-[1px] rounded-md opacity-50"
+          style={{
+            background:
+              "conic-gradient(#ff4545, #00ff99, #006aff, #ff0095, #ff4545)",
+            filter: pulsingBg,
+          }}
+        />
+      </div>
+    </main>
   );
 }
